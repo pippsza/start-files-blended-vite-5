89e7a6f0c9a92f55fb5dd4fc359eb5a3876f8443
pippsza <bastahov18@gmail.com>
2025-04-18 20:52:49 +0300

    2-steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
-import Heading from './components/Heading/Heading';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './pages/Home';
+import Rates from './pages/Rates';
+import { useEffect } from 'react';
+import { getUserInfo } from './service/opencagedataApi';
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    async function success(pos) {
+      const crd = pos.coords;
+
+      // console.log('Your current position is:');
+      // console.log(`Latitude : ${crd.latitude}`);
+      // console.log(`Longitude: ${crd.longitude}`);
+      // console.log(`More or less ${crd.accuracy} meters.`);
+      const userInfo = await getUserInfo(crd);
+      const iso = userInfo.results[0].annotations.currency.iso_code;
+      console.log(iso);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+        <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
+      </Route>
+    </Routes>
+  );
 };
